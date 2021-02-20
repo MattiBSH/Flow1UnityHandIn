7ee99bd631f4abfe9bce1fb704041959dde898d9
MattiBSH <cph-mh829@cphbusiness.dk>
2021-02-20 17:15:55 +0100

    justeringer pga antal bolde og fordi der kommer for mange bolde ned i vulkan

--- Flow1HandIn/Assets/ObstacleSpawner2.cs
+++ Flow1HandIn/Assets/ObstacleSpawner2.cs
@@ -20,10 +20,10 @@ public class ObstacleSpawner2 : MonoBehaviour
     time += Time.deltaTime;
     float heightOfPlayer =player.transform.position.y;
     //adjusting for height to mak difficulty higher when getting close to goal
-     if (time >= (0.3-((heightOfPlayer/10)/20))) {
+     if (time >= (0.4-((heightOfPlayer/10)/31))) {
        time = 0.0f;
     //make Random x z y 
-    x = Random.Range(252, 333); z = Random.Range(235, 286); y = Random.Range(60, 70); 
+    x = Random.Range(252, 333); z = Random.Range(235, 286); y = Random.Range(69, 75); 
     //variable for deployment of specific object
     t = Random.Range(1,3);
     //put spawn in random position
